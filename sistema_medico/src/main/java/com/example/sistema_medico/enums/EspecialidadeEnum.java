613e2e0d62e7876cc6507584c9249b67fa14018c
João Pedro <pedro.fernandes1058@gmail.com>
2025-04-21 23:51:46 -0300

    feat: entidade medico

---
+++ sistema_medico/src/main/java/com/example/sistema_medico/enums/EspecialidadeEnum.java
@@ -0,0 +1,9 @@
+package com.example.sistema_medico.enums;
+
+public enum EspecialidadeEnum {
+    CARDIOLOGIA,
+    DERMATOLOGIA,
+    ORTOPEDIA,
+    PEDIATRIA,
+    CLINICA_GERAL
+}
---
+++ sistema_medico/src/main/java/com/example/sistema_medico/model/Medico.java
@@ -0,0 +1,35 @@
+package com.example.sistema_medico.model;
+
+import com.example.sistema_medico.enums.EspecialidadeEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "medicos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "crm", nullable = false, unique = true)
+    private String crm;
+
+    @Column(name= "email", nullable = false)
+    private String email;
+
+    @Column(name= "telefone")
+    private String telefone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "especialidade", nullable = false)
+    private EspecialidadeEnum especialidade;
+}
