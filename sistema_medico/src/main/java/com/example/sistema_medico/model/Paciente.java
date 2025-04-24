221c9dea09ee51e0df019f65699d670af5800afe
João Pedro <pedro.fernandes1058@gmail.com>
2025-04-23 23:29:31 -0300

    feat: entidade paciente

---
+++ sistema_medico/src/main/java/com/example/sistema_medico/model/Paciente.java
@@ -0,0 +1,35 @@
+package com.example.sistema_medico.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pacientes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String cpf;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+}
