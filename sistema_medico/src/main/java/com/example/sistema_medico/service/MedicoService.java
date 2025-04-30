b055de75316afe3e06e427855067a7fb8cfc8bf4
João Pedro <pedro.fernandes1058@gmail.com>
2025-04-29 23:43:14 -0300

    feat: medico service terminado

--- sistema_medico/src/main/java/com/example/sistema_medico/service/MedicoService.java
+++ sistema_medico/src/main/java/com/example/sistema_medico/service/MedicoService.java
@@ -5,6 +5,8 @@ import com.example.sistema_medico.repository.MedicoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class MedicoService {
@@ -14,4 +16,33 @@ public class MedicoService {
     public Medico medicoSave(Medico medico) {
         return medicoRepository.save(medico);
     }
+
+    public List<Medico> listarMedicos(){
+        return medicoRepository.findAll();
+    }
+
+    public Medico buscarMedicoId(Long id){
+        return medicoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Medico não encontrado"));
+    }
+
+    public Medico atualizarMedico(Medico medico){
+        Medico medicoExistente = buscarMedicoId(medico.getId());
+
+        medicoExistente.setNome(medico.getNome());
+        medicoExistente.setCrm(medico.getCrm());
+        medicoExistente.setEmail(medico.getEmail());
+        medicoExistente.setTelefone(medico.getTelefone());
+        medicoExistente.setEspecialidade(medico.getEspecialidade());
+        medicoExistente.setConsultas(medico.getConsultas());
+
+        return medicoRepository.save(medicoExistente);
+    }
+
+    public void deletarMedico(Long id){
+        Medico medico = buscarMedicoId(id);
+        medicoRepository.delete(medico);
+    }
+
+
 }
