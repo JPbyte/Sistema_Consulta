d727890b137c235119695c0961a82ccce568e0ac
João Pedro <pedro.fernandes1058@gmail.com>
2025-05-02 23:34:14 -0300

    feat: listarPacientes controller

--- sistema_medico/src/main/java/com/example/sistema_medico/controller/PacienteController.java
+++ sistema_medico/src/main/java/com/example/sistema_medico/controller/PacienteController.java
@@ -6,10 +6,9 @@ import com.example.sistema_medico.service.PacienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +22,12 @@ public class PacienteController {
         Paciente paciente1 = pacienteService.pacienteSave(paciente);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(paciente1);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Paciente>> listarPacientes(){
+        List<Paciente> pacientesListados = pacienteService.listarPacientes();
+
+        return ResponseEntity.ok(pacientesListados);
     }
 }
